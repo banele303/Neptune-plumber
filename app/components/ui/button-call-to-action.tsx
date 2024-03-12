144ded2ac1138868c1234df3f54e674f8d4cb5e0
blessed <sibandablessed724@gmail.com>
2024-03-12 08:21:08 +0000

    First commit

--- app/components/ui/button-call-to-action.tsx
+++ app/components/ui/button-call-to-action.tsx
@@ -3,24 +3,28 @@ import React from "react";
 import { Boxes } from "./background-boxes";
 import { cn } from "../utils/cn";
 import { BookCallAppointment } from "./form";
+import Link from "next/link";
 
-export default function ButtonCallToAction ({phoneNumber}) {
-  const handleButtonClick = () => {
-    window.location.href =`tell:${phoneNumber}`;
-  };
 
+
+export default function ButtonCallToAction () {
+
+  const phoneNumber = '+27 83 300 0705';
   return (
    
 <div className="">
        <div className="flex flex-col justify-start md:mt-6 px-4 ">
-            <button onClick={handleButtonClick} className="bg-blue-800 no-underline group   cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block">
+            <button  className="bg-blue-800 no-underline group   cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block">
               <span className="absolute inset-0 overflow-hidden rounded-full">
                 <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
               </span>
-              <div className="relative flex space-x-2 items-center z-10 rounded-full bg-orange-600 py-4 px-4 ring-1 ring-white/10">
-                <span className="text-[17px] text-center">
-                  CALL NOW :+27 83 300 0705
-                </span>
+              <div className="relative flex space-x-2 items-center z-10 rounded-full bg-orange-600  ring-1 ring-white/10">
+              <Link
+        href={`tel:${phoneNumber}`}
+        className="py-4 px-4"
+      >
+        Call Now : {phoneNumber}
+      </Link>
                 <svg
                   fill="none"
                   height="16"
--- app/components/ui/form.tsx
+++ app/components/ui/form.tsx
@@ -2,33 +2,28 @@
 import React from "react";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
-
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
+import { useForm, ValidationError } from '@formspree/react';
 import { cn } from "../utils/cn";
+import NewForm from "./NewForm";
 
 export function BookCallAppointment() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form submitted");
-  };
+  
   return (
     <div className="max-w-md w-full mx-auto  rounded-2xl mt-[4rem] md:p-8 shadow-input bg-black dark:bg-white py-8 px-5">
       <h2 className="font-bold text-xl text-slate-300 dark:text-neutral-200">
         Welcome to Neptune Plumbing
       </h2>
       <p className="text-slate-300 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Tell us more about what you are looking for
-      </p>
+        Tell us more about what you are looking for</p>
+      <NewForm/>
+{/* 
 
-      <form className="my-8" onSubmit={handleSubmit}>
+<form className="my-8" onSubmit={handleSubmit}>
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <Label htmlFor="firstname">First name</Label>
             <Input id="firstname" placeholder="Tyler" type="text" />
+            
           </LabelInputContainer>
           <LabelInputContainer>
             <Label htmlFor="lastname">Last name</Label>
@@ -42,6 +37,11 @@ export function BookCallAppointment() {
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
           <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+          <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
         </LabelInputContainer>
 
         <textarea
@@ -60,17 +60,25 @@ export function BookCallAppointment() {
           rows={3} // Adjust the number of rows as needed
         // Adjust the width (number of characters) as needed
         />
-
+<ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
 
         <button
+        type="submit" disabled={state.submitting}
           className="bg-gradient-to-br  relative group/btn from-black dark:from-yellow-700 dark:to-yellow-900 to-orange-600 block dark:bg-orange-800 w-full text-white mt-7 rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-          type="submit"
+    
         >
           Send
           <BottomGradient />
         </button>
 
       </form>
+
+*/}
+      
     </div>
   );
 }
