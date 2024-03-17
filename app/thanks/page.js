8cf88b1f1740cd865b3b6bf17b2d9e4267b87fc6
blessed <sibandablessed724@gmail.com>
2024-03-17 05:53:03 +0000

    form tag

--- app/thanks/page.js
+++ app/thanks/page.js
@@ -1,6 +1,6 @@
 
 import Footer from '../our-plumbing-services/ui/footer';
-
+import Head from 'next/head';
 import { Navbar } from '../components/ui/Navbar';
 import Thanks from './thanks';
 
@@ -12,6 +12,17 @@ import Thanks from './thanks';
 export default function PrivacyPolicy() {
   return (
     <div>
+
+<Head>
+        {/* Google Tag Manager script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              gtag('event', 'conversion', {'send_to': 'AW-11405523497/bEKRCPf7qpwZEKn0yb4q'});
+            `,
+          }}
+        />
+      </Head>
       <Navbar/>
    <Thanks/>
      <Footer/>
